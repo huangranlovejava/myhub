4963f9f3d4c04501dc4a9f3e52aedfab9e2f471a
helloworld <394530295@qq.com>
2018-12-13 09:34:55 +0800

    实现通用Mapper和分页助手功能

--- sencond/pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
+++ sencond/pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
@@ -6,6 +6,7 @@ import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,7 +18,19 @@ public class BrandController {
     private BrandService brandService;
 
     @GetMapping("/findAll")
-    public List<TbBrand> queryAll(){
-        return brandService.queryAll();
+    public List<TbBrand> queryAll() {
+        //  return brandService.queryAll();
+        return brandService.findAll();
     }
+
+    @GetMapping("/testPage")
+    public List<TbBrand> testPage(@RequestParam(value = "page", defaultValue = "1") Integer page,
+                                  @RequestParam(value = "rows", defaultValue = "10") Integer rows) {
+
+       // return brandService.testPage(page, rows);
+        return (List<TbBrand>) brandService.findPage(page,rows).getRows();
+
+    }
+
+
 }
---
+++ sencond/pinyougou/pinyougou-pojo/src/main/java/com/pinyougou/service/BaseService.java
@@ -0,0 +1,32 @@
+package com.pinyougou.service;
+
+import com.pinyougou.vo.PageResult;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface BaseService<T> {
+    /**
+     * 通过id查询
+     *
+     * @param id
+     * @return
+     */
+    T findOne(Serializable id);
+
+    List<T> findAll();
+
+    List<T> findByWhere(T t);
+
+    void add(T t);
+
+    void update(T t);
+
+    void deleteByIds(Serializable[] ids);
+
+    PageResult findPage(Integer page, Integer rows);
+
+    PageResult findPage(Integer page, Integer rows, T t);
+
+
+}
